13c47d6e652b3eb7a205efb150637c56e7703009
Chris Cocker <ccocker@hotmail.ca>
2016-03-30 11:17:36 -0700

    Initial Prototype files uploaded

---
+++ BearWatch/www/js/controllers.js
@@ -0,0 +1,106 @@
+angular.module('app.controllers', [])
+  
+.controller('cameraCtrl', function($scope) {
+
+})
+   
+.controller('log41245PictureTakenCtrl', function($scope) {
+
+})
+   
+.controller('environmentCtrl', function($scope) {
+
+})
+      
+.controller('startCtrl', function($scope) {
+
+})
+   
+.controller('log11234Bear1SpottedCtrl', function($scope) {
+
+})
+   
+.controller('newSessionObserversCtrl', function($scope) {
+
+})
+   
+.controller('reviewListCtrl', function($scope) {
+
+})
+   
+.controller('addCommentCtrl', function($scope) {
+
+})
+   
+.controller('reviewSessionCtrl', function($scope) {
+
+})
+   
+.controller('reviewSession2Ctrl', function($scope) {
+
+})
+   
+.controller('newSessionLocationCtrl', function($scope) {
+
+})
+   
+.controller('newSessionProtocolModeCtrl', function($scope) {
+
+})
+   
+.controller('newSessionEnvironmentVariablesCtrl', function($scope) {
+
+})
+   
+.controller('newSessionObserverSVisibilityCtrl', function($scope) {
+
+})
+   
+.controller('bear1SpecificationsCtrl', function($scope) {
+
+})
+   
+.controller('addNewBearCtrl', function($scope) {
+
+})
+   
+.controller('newSessionFoodSourcesCtrl', function($scope) {
+
+})
+   
+.controller('newSessionModeCtrl', function($scope) {
+
+})
+   
+.controller('additionalObserversCtrl', function($scope) {
+
+})
+   
+.controller('focalCtrl', function($scope) {
+
+})
+   
+.controller('feedingForagingCtrl', function($scope) {
+
+})
+   
+.controller('nonInteractiveCtrl', function($scope) {
+
+})
+   
+.controller('bearBearInteractionCtrl', function($scope) {
+
+})
+   
+.controller('bearHumanInteractionCtrl', function($scope) {
+
+})
+   
+.controller('humanCtrl', function($scope) {
+
+})
+   
+.controller('pageCtrl', function($scope) {
+
+})
+ 
---
+++ BearWatch/www/js/routes.js
@@ -0,0 +1,245 @@
+angular.module('app.routes', ['ionicUIRouter'])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('tabsController.camera', {
+    url: '/page10',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/camera.html',
+        controller: 'cameraCtrl'
+      }
+    }
+  })
+
+  .state('log41245PictureTaken', {
+    url: '/page17',
+    templateUrl: 'templates/log41245PictureTaken.html',
+    controller: 'log41245PictureTakenCtrl'
+  })
+
+  .state('tabsController.environment', {
+    url: '/page3',
+    views: {
+      'tab3': {
+        templateUrl: 'templates/environment.html',
+        controller: 'environmentCtrl'
+      }
+    }
+  })
+
+  .state('tabsController', {
+    url: '/page1',
+    templateUrl: 'templates/tabsController.html',
+    abstract:true
+  })
+
+  .state('start', {
+    url: '/page2',
+    templateUrl: 'templates/start.html',
+    controller: 'startCtrl'
+  })
+
+  .state('log11234Bear1Spotted', {
+    url: '/page15',
+    templateUrl: 'templates/log11234Bear1Spotted.html',
+    controller: 'log11234Bear1SpottedCtrl'
+  })
+
+  .state('newSessionObservers', {
+    url: '/page6',
+    templateUrl: 'templates/newSessionObservers.html',
+    controller: 'newSessionObserversCtrl'
+  })
+
+  .state('reviewList', {
+    url: '/page7',
+    templateUrl: 'templates/reviewList.html',
+    controller: 'reviewListCtrl'
+  })
+
+  .state('addComment', {
+    url: '/page16',
+    templateUrl: 'templates/addComment.html',
+    controller: 'addCommentCtrl'
+  })
+
+  /* 
+    The IonicUIRouter.js UI-Router Modification is being used for this route.
+    To navigate to this route, do NOT use a URL. Instead use one of the following:
+      1) Using the ui-sref HTML attribute:
+        ui-sref='tabsController.reviewSession'
+      2) Using $state.go programatically:
+        $state.go('tabsController.reviewSession');
+    This allows your app to figure out which Tab to open this page in on the fly.
+    If you're setting a Tabs default page or modifying the .otherwise for your app and
+    must use a URL, use one of the following:
+      /page1/tab2/page14
+      /page1/tab4/page14
+      /page1/tab3/page14
+  */
+  .state('tabsController.reviewSession', {
+    url: '/page14',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/reviewSession.html',
+        controller: 'reviewSessionCtrl'
+      },
+      'tab4': {
+        templateUrl: 'templates/reviewSession.html',
+        controller: 'reviewSessionCtrl'
+      },
+      'tab3': {
+        templateUrl: 'templates/reviewSession.html',
+        controller: 'reviewSessionCtrl'
+      }
+    }
+  })
+
+  .state('reviewSession2', {
+    url: '/page25',
+    templateUrl: 'templates/reviewSession2.html',
+    controller: 'reviewSession2Ctrl'
+  })
+
+  .state('newSessionLocation', {
+    url: '/page8',
+    templateUrl: 'templates/newSessionLocation.html',
+    controller: 'newSessionLocationCtrl'
+  })
+
+  .state('newSessionProtocolMode', {
+    url: '/page10',
+    templateUrl: 'templates/newSessionProtocolMode.html',
+    controller: 'newSessionProtocolModeCtrl'
+  })
+
+  .state('newSessionEnvironmentVariables', {
+    url: '/page11',
+    templateUrl: 'templates/newSessionEnvironmentVariables.html',
+    controller: 'newSessionEnvironmentVariablesCtrl'
+  })
+
+  .state('newSessionObserverSVisibility', {
+    url: '/page18',
+    templateUrl: 'templates/newSessionObserverSVisibility.html',
+    controller: 'newSessionObserverSVisibilityCtrl'
+  })
+
+  .state('tabsController.bear1Specifications', {
+    url: '/page23',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/bear1Specifications.html',
+        controller: 'bear1SpecificationsCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.addNewBear', {
+    url: '/page22',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/addNewBear.html',
+        controller: 'addNewBearCtrl'
+      }
+    }
+  })
+
+  .state('newSessionFoodSources', {
+    url: '/page12',
+    templateUrl: 'templates/newSessionFoodSources.html',
+    controller: 'newSessionFoodSourcesCtrl'
+  })
+
+  .state('newSessionMode', {
+    url: '/page20',
+    templateUrl: 'templates/newSessionMode.html',
+    controller: 'newSessionModeCtrl'
+  })
+
+  .state('additionalObservers', {
+    url: '/page9',
+    templateUrl: 'templates/additionalObservers.html',
+    controller: 'additionalObserversCtrl'
+  })
+
+  .state('tabsController.focal', {
+    url: '/page21',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/focal.html',
+        controller: 'focalCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.feedingForaging', {
+    url: '/page13',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/feedingForaging.html',
+        controller: 'feedingForagingCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.nonInteractive', {
+    url: '/page24',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/nonInteractive.html',
+        controller: 'nonInteractiveCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.bearBearInteraction', {
+    url: '/page26',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/bearBearInteraction.html',
+        controller: 'bearBearInteractionCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.bearHumanInteraction', {
+    url: '/page27',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/bearHumanInteraction.html',
+        controller: 'bearHumanInteractionCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.human', {
+    url: '/page28',
+    views: {
+      'tab4': {
+        templateUrl: 'templates/human.html',
+        controller: 'humanCtrl'
+      }
+    }
+  })
+
+  .state('page', {
+    url: '/page19',
+    templateUrl: 'templates/page.html',
+    controller: 'pageCtrl'
+  })
+
+$urlRouterProvider.otherwise('/page2')
+
+  
+
+});
