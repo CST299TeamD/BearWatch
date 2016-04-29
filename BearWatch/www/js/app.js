461bf398519d0bce24f252f3f49ffaa153ece621
Chris Cocker <ccocker@hotmail.ca>
2016-04-29 10:16:29 -0700

    SQLite test DB running! still need to build more structure

--- BearWatch/www/js/app.js
+++ BearWatch/www/js/app.js
@@ -1,7 +1,5 @@
 // Ionic Starter App
 
-//global DB Variable
-var db = null;
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
@@ -20,21 +18,6 @@ angular.module('app', ['ionic', 'ngCordova', 'app.controllers', 'app.routes', 'a
     if(window.StatusBar) {
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
-    }
-                       
-                       db = $cordovaSQLite.openDB({ name: "BW.db" });
-                       $cordovaSQLite.execute(db, "CREATE TABLE IF NOT EXISTS sessions ("
-                                              + "session_id       INTEGER PRIMARY KEY NOT NULL, "
-                                              + "protocol          TEXT    NOT NULL, "
-                                              + "collection_mode   TEXT    NOT NULL, "
-                                              + "park              TEXT    NOT NULL, "
-                                              + "water_body text   TEXT    NOT NULL, "
-                                              + "flow              TEXT    NOT NULL, "
-                                              + "start_time        TEXT    NOT NULL, "
-                                              + "finish_time       TEXT    NOT NULL, "
-                                              + "water_clarity     TEXT    NOT NULL, "
-                                              + "water_temp        TEXT    NOT NULL, "
-                                              + "observers         TEXT    NOT NULL);")
-                        
+    }	  
   });
 })
--- BearWatch/www/js/controllers.js
+++ BearWatch/www/js/controllers.js
@@ -33,35 +33,50 @@ angular.module('app.controllers', [])
 })
 
 .controller( 'dbTest', function ($scope, $cordovaSQLite){
-	$scope.result = "no result";
 	
-    $scope.insert = function(name) {
-        var query = "INSERT INTO sessions (observers) VALUES (?)";
-        $cordovaSQLite.execute(db, query, [name]).then(function(res) {
-            console.log("INSERT ID -> " + res.insertId);
-			$scope.result = "inserted";
-        }, function (err) {
-            console.error(err);
-			$scope.result = "insert failed";
-        });
-    }
- 
+	window.sqlitePlugin.deleteDatabase({name: 'my.db', location: 'default'});
+	var db = window.sqlitePlugin.openDatabase({name: 'bw.db', location: 'default'});
+
+	db.transaction(function(tx) {
+		tx.executeSql(
+			"CREATE TABLE IF NOT EXISTS sessions ("
+			+ "session_id       INTEGER PRIMARY KEY NOT NULL, "
+			+ "protocol          TEXT    , "
+			+ "collection_mode   TEXT    , "
+			+ "park              TEXT    , "
+			+ "water_body text   TEXT    , "
+			+ "flow              TEXT    , "
+			+ "start_time        TEXT    , "
+			+ "finish_time       TEXT    , "
+			+ "water_clarity     TEXT    , "
+			+ "water_temp        TEXT    , "
+			+ "observers         TEXT);"
+		);
+	});
+	
+	$scope.insert = function(name) {
+		db.transaction(function(tx) {
+			tx.executeSql("INSERT INTO sessions (observers) VALUES (?)", [name], function(tx, res) {
+				console.log("BW insert happening");
+				console.log("insertId: " + res.insertId + " -- probably 1");
+				console.log("rowsAffected: " + res.rowsAffected + " -- should be 1");				
+			});
+		}, function(e) {
+			console.log("ERROR: " + e.message);
+		});
+	}
+	
     $scope.select = function(name) {
-        var query = "SELECT observers FROM sessions WHERE observers = ?";
-        $cordovaSQLite.execute(db, query, [name]).then(function(res) {
-            if(res.rows.length > 0) {
-                console.log("SELECTED -> " + res.rows.item(0).observers);
-				$scope.result =  res.rows.item(0).observers;
-            } else {
-                console.log("No results found");
-				$scope.result =  "No results found";
-            }
-        }, function (err) {
-            console.error(err);
-			$scope.result = "select failed";
-        });
-    }
-            
+		db.transaction(function(tx) {
+			tx.executeSql("select count(observers) as cnt from sessions;", [], function(tx, res) {
+				console.log("BW select happening");
+				console.log("res.rows.length: " + res.rows.length + " -- should be 1");
+				console.log("res.rows.item(0).cnt: " + res.rows.item(0).cnt + " -- should be 1");
+			});
+		}, function(e) {
+			console.log("ERROR: " + e.message);
+		});
+    }      
 })
 
 .controller('focalTabCommentCtrl', function($scope) {
