f2abd29e95d2c509b95ab55223e1700cc922f4fa
Chris Cocker <ccocker@hotmail.ca>
2016-04-29 17:03:40 -0700

    More DB test code...

--- BearWatch/www/js/controllers.js
+++ BearWatch/www/js/controllers.js
@@ -57,29 +57,37 @@ angular.module('app.controllers', [])
 	
 	$scope.insert = function(name) {
             console.log("MAC INSERT HAPPENING");
-            $scope.result = "MAC INSERT HAPPENING";
 		db.transaction(function(tx) {
 			tx.executeSql("INSERT INTO sessions (observers) VALUES (?)", [name], function(tx, res) {
 				console.log("BW insert happening");
+                          $scope.result = "MAC INSERT HAPPENING: ";
+                          for (var key in tx){
+                            $scope.result += key + " : " + tx[key];
+                          }
 				console.log("insertId: " + res.insertId + " -- probably 1");
 				console.log("rowsAffected: " + res.rowsAffected + " -- should be 1");				
 			});
 		}, function(e) {
 			console.log("ERROR: " + e.message);
+                       $scope.result = "ERROR " + e.message;
 		});
 	}
 	
     $scope.select = function(name) {
             console.log("MAC SELECT HAPPENING");
-            $scope.result = "MAC SELECT HAPPENING";
 		db.transaction(function(tx) {
-			tx.executeSql("select count(observers) as cnt from sessions;", [], function(tx, res) {
+			tx.executeSql("select observers from sessions;", [], function(tx, res) {
 				console.log("BW select happening");
+                          $scope.result = "MAC SELECT HAPPENING ";
+                          for (var key in res){
+                          $scope.result += key + " : " + res[key];
+                          }
 				console.log("res.rows.length: " + res.rows.length + " -- should be 1");
-				console.log("res.rows.item(0).cnt: " + res.rows.item(0).cnt + " -- should be 1");
+                          $scope.result += " Result: " + res.rows.item(1).session_id;
 			});
 		}, function(e) {
 			console.log("ERROR: " + e.message);
+                       $scope.result = "ERROR: " + e.message;
 		});
     }      
 })
