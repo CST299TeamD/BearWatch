4d44c2613d4854134f260585d394b29a322413e1
Chris Cocker - MacMini <ccocker@hotmail.ca>
2016-05-12 17:37:58 -0700

    Database updated - creating in .run function. new test samples

--- BearWatch/www/js/app.js
+++ BearWatch/www/js/app.js
@@ -1,5 +1,8 @@
 // Ionic Starter App
 
+//global variables
+var db;
+var debug = true;
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
@@ -10,11 +13,37 @@ angular.module('app', ['ionic', 'ngCordova', 'app.controllers', 'app.routes', 'a
 
 .run(function($ionicPlatform, $cordovaSQLite, $cordovaCamera, $cordovaFile, $cordovaEmailComposer, $cordovaFileTransfer) {
   $ionicPlatform.ready(function() {
-                       //global scope variables
-                       
-                       //set true for debugging
-                       $scope.debug = false;
-                       
+  
+    //setup database schema
+    db = $cordovaSQLite.openDB({name:"bear_watch.db", location:'default'});
+
+    /***** Lookup Tables ******/
+
+    //park names table
+    $cordovaSQLite.execute(db, 
+      "CREATE TABLE IF NOT EXISTS lv_parks ("
+      + "park_id       INTEGER PRIMARY KEY NOT NULL, "
+      + "park_name         TEXT);"
+    );
+
+    /****** Storage Tables ******/
+
+    //sessions table
+    $cordovaSQLite.execute(db, 
+      "CREATE TABLE IF NOT EXISTS sessions ("
+      + "session_id       INTEGER PRIMARY KEY NOT NULL, "
+      + "observers         TEXT    , "
+      + "park              TEXT    , "
+      + "park_site         TEXT    , "
+      + "protocol          TEXT    , "
+      + "stationary        TEXT    , "
+      + "zone_type         TEXT    , "
+      + "zone_comment      TEXT    , "
+      + "start_time        TEXT    , "
+      + "finish_time       TEXT    , "
+      + "collection_mode   TEXT);"
+    );
+
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     if(window.cordova && window.cordova.plugins.Keyboard) {
@@ -23,6 +52,6 @@ angular.module('app', ['ionic', 'ngCordova', 'app.controllers', 'app.routes', 'a
     if(window.StatusBar) {
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
-    }	  
+    }  
   });
 })
