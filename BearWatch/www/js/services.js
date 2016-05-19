19fb1fca8a437e53bfd7ee3f766d39bd7dbfe66b
Chris Cocker <ccocker@hotmail.ca>
2016-05-18 19:10:22 -0700

    Test DB code - not finished

--- BearWatch/www/js/services.js
+++ BearWatch/www/js/services.js
@@ -80,6 +80,50 @@ angular.module('app.services', [])
 		} 
 	};
     
+    //return environment object
     return Enviro;
 
+}])
+
+.factory('DB', [function($cordovaSQLite, $ionicPlatform, $q){
+    
+    var self = this;
+    console.log("$q.defer()3: " + $q.defer());
+    // Handle query's and potential errors
+    self.query = function (query, parameters) {
+        parameters = parameters || [];
+        console.log("$q.defer()4: " + $q.defer());
+        var q = $q.defer();
+
+        $ionicPlatform.ready(function () {
+        $cordovaSQLite.execute(db, query, parameters)
+            .then(function (result) {
+            q.resolve(result);
+            }, function (error) {
+            console.warn('I found an error');
+            console.warn(error);
+            q.reject(error);
+            });
+        });
+        return q.promise;
+    }
+
+    // Proces a result set
+    self.getAll = function(result) {
+        var output = [];
+
+        for (var i = 0; i < result.rows.length; i++) {
+        output.push(result.rows.item(i));
+        }
+        return output;
+    }
+
+    // Proces a single result
+    self.getById = function(result) {
+        var output = null;
+        output = angular.copy(result.rows.item(0));
+        return output;
+    }
+
+    return self;
 }]);
