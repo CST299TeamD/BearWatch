e630107c060791e6fc8ad17a0059eb84a6b29354
Chris Cocker - MacMini <ccocker@hotmail.ca>
2016-05-23 02:56:54 -0700

    Camera tab complete - edit functionality added

--- BearWatch/www/js/controllers/commentCameraControl.js
+++ BearWatch/www/js/controllers/commentCameraControl.js
@@ -3,7 +3,6 @@ angular.module('app.controllers')
 .controller('tabCommentCtrl', function($scope, Comment, $ionicPopup) {
 	$scope.Comment = Comment;
 
-
 	//function to verify user comment edit (may overwrite unsaved text field)
 	$scope.editRequest = function (id){
 		if(Comment.text != '' && Comment.text != null){
@@ -29,88 +28,64 @@ angular.module('app.controllers')
 	};
 })
 
-.controller('tabCameraCtrl', function($scope, $cordovaCamera, $cordovaFile, $cordovaSQLite) {
-	$scope.camResult = "Photo page initialized";
+.controller('tabCameraCtrl', function($scope, $ionicPopup, Picture) {
+	$scope.debug = debug;
+
+	//picture object
+	$scope.Picture = Picture;
             
 	//function for taking picture using device camera
 	$scope.takePhoto = function () {
-		$scope.cameraResult = "taking photo";
 		
-		var options = {
-		quality: 75,
-		destinationType: Camera.DestinationType.DATA_URL,
-		sourceType: Camera.PictureSourceType.CAMERA,
-		allowEdit: true,
-		encodingType: Camera.EncodingType.JPEG,
-		targetWidth: 300,
-		targetHeight: 300,
-		popoverOptions: CameraPopoverOptions,
-		saveToPhotoAlbum: false
-		};
-
-		$cordovaCamera.getPicture(options).then(function (imageData) {
-			$scope.cameraResult ="taking photo pt.2";
-			$scope.imgURI = "data:image/jpeg;base64," + imageData;
-			$scope.imageInfo = imageData;
+		Picture.take().then(function(result){
+			console.log("Photo taken");
+			$scope.imgURI = result.imgURI;
 		}, function (err) {
 			// An error occured
-			$scope.cameraResult = "Camera error: " + err;
+			console.log("Camera error: " + err);
 		});
-	}
+	};
 	
 	//save photo to session - TODO: Session id, timestamp, GPS
 	$scope.choosePhoto = function () {
 		
 		$scope.insertResult = "Initialized";
 
-/*		//Delete all from logs table.
-		$cordovaSQLite.execute(db, 'DELETE FROM logs')
-        .then(function(result) {
-            $scope.deleteResult = "deleted";
-        }, function(error) {
-            $scope.deleteResult = "Error on delete: " + error.message;
-            return;
-        })
-*/
+		Picture.save().then(function(result){
+			$scope.discardPhoto();
+		}, function(err){
+			console.log("Photo save error: " + err);
+		});
+	};
 
-		//Put a single picture taken into logs table. TODO: Session id, timestamps, GPS
-		$cordovaSQLite.execute(db, 'INSERT INTO logs (picture_data) VALUES (?)', [$scope.imageInfo])
-        .then(function(result) {
-            $scope.insertResult = "Picture insert successful! Probably...";
-        }, function(error) {
-            $scope.insertResult = "Error on saving: " + error.message;
-            return;
-        })
+	//function to edit photo
+	$scope.editPicture = function(id){
+		if($scope.imgURI !== undefined){
+			var confirmPopup = $ionicPopup.confirm({
+				title: 'Edit Photo',
+				template: '<h1>Warning</h1> <p>Unsaved photo detected. Editing this this will overwrite unsaved text, are you sure?</p>',
+				cssClass: 'commentPopup'
 
-		//Test Select of pictures
-		if(debug){
-			$scope.selectResult = "Initialized";
-			$cordovaSQLite.execute(db, "SELECT log_id, picture_data FROM logs").then(
-			function(result) {
-				//$scope.selectResult = "Select successful!";
-				$scope.selectResult += "...Select successful! Rows length = " + result.rows.length;
-				if (result.rows.length > 0) {
-					$scope.fileName = "Select successful!";
-					var i=0;
-					while(i < result.rows.length){
-						$scope.selectResult += "...log_id: "+result.rows.item(i).log_id;//+"...Picture_data: "+result.rows.item(i).picture_data;
-						i++;
-					}
+			});
+
+		   confirmPopup.then(function(res) {
+				if(res) {
+					console.log('Photo edit confirmed');
+					$scope.imgURI = Picture.edit(id);
 				} else {
-					$scope.selectResult += "...No rows found"
+					console.log('Photo edit aborted');
 				}
-			},
-			function(error) {
-				$scope.selectResult = "Error on loading: " + error.message;
-			})
+			});
+		}else{
+			console.log('Here we go - Photo edit ' + id);
+			$scope.imgURI = Picture.edit(id);
 		}
-	}
+	};
 	
-	//discard photo - TODO: fix this...
+	//function to discard photo
 	$scope.discardPhoto = function () {
-		$scope.imgURI;
-		$scope.cameraResult = "Clearing photo - reloading screen";
-		$state.go($state.current, {}, {reload: true});
-	}
+		$scope.imgURI = undefined;
+		Picture.clear();
+	};
 
 });
---
+++ BearWatch/www/js/services/picture.js
@@ -0,0 +1,172 @@
+angular.module('app.services')
+
+.factory('Picture', function($cordovaCamera, $cordovaFile, $cordovaSQLite, Session, $q, BearList){
+	
+	//keep track of session pictures
+	var count = 0;
+
+	//picture object
+	var Picture = {
+		fileName: '',
+		subjects: [],
+		comment: '',
+		imgURI: '',
+		imgInfo: '',
+		pictures: []
+	};
+
+	//function to capture image
+	Picture.take = function(){
+		
+		var defer = $q.defer();
+
+		//update subject list for checkbox vaules
+		Picture.subjects = [];
+		for(bear in BearList.add){
+			console.log("bear list value: " + BearList.add[bear].name);
+			var bearObj = {id: BearList.add[bear].id, name: BearList.add[bear].name, selected: false};
+			Picture.subjects.push(bearObj);
+		}
+		
+		var options = {
+			quality: 100,
+			destinationType: Camera.DestinationType.DATA_URL,
+			sourceType: Camera.PictureSourceType.CAMERA,
+			allowEdit: true,
+			encodingType: Camera.EncodingType.JPEG,
+			//targetWidth: 300,
+			//targetHeight: 300,
+			popoverOptions: CameraPopoverOptions,
+			saveToPhotoAlbum: false
+		};
+
+		$cordovaCamera.getPicture(options).then(function (imageData) {
+			console.log("picture success");
+			Picture.imgURI = "data:image/jpeg;base64," + imageData;
+			Picture.imgInfo = imageData;
+			count ++;
+			Picture.fileName = Session.park + '-' +  Session.site + '-' + count;
+			defer.resolve(Picture);
+		}, function (err) {
+			// An error occured
+			console.log("Camera error: " + err);
+			defer.reject(err);
+		});
+
+		return defer.promise;
+	};
+
+	//function to save picture and associated info
+	Picture.save = function(){
+		
+		var defer = $q.defer();
+
+		//find the selected subjects
+		var subjects = [];
+		for(subject in Picture.subjects){
+			if(Picture.subjects[subject].selected){
+				subjects.push(Picture.subjects[subject].name)
+			}
+		}
+
+		console.log("Subjects: " + subjects.toString());
+
+		//Save picture & data to logs table
+		$cordovaSQLite.execute(db, 
+			'INSERT INTO logs (timestamp, session_id, comment_type, comment, picture_subjects, picture_data)'
+		+ 	' VALUES (?, ?, ?, ?, ?, ?)', 
+			[new Date().toLocaleTimeString(), Session.id, 'picture-comment', Picture.comment, subjects.toString(), Picture.imgInfo])
+        .then(function(result) {
+            console.log("Picture insert successful!");
+            Picture.pictures.push({
+            	id: result.insertId, 
+            	fileName: Picture.fileName, 
+            	imgInfo: Picture.imgInfo, 
+            	imgURI: Picture.imgURI, 
+            	subjects: Picture.subjects, 
+            	comment: Picture.comment, 
+            	subjectString: subjects.toString()
+            });
+            defer.resolve(result);
+        }, function(error) {
+            console.log("Error on saving: " + error.message);
+            defer.reject(error);
+        });
+
+        return defer.promise;
+	};
+
+	//function to clear picture results
+	Picture.clear = function (id){
+		console.log("picture id: " + id);
+		
+		//remove log from table if is was passed
+		if(id !== undefined){
+			
+			//remove from picture object
+	        var index = -1;
+	        for(i = 0; i < Picture.pictures.length; i++) {
+	            if (Picture.pictures[i].id == id) {
+	                index = i;
+	                break;
+	            }
+	        }
+	        if(index > -1){
+	            Picture.pictures.splice(index, 1);
+	        }
+
+	        //remove from DB
+	        $cordovaSQLite.execute(db, 'DELETE FROM logs WHERE log_id = (?)',[id])
+	        .then(function(result) {
+	            console.log("Picture delete successful");
+	        }, function(error) {
+	            console.log("Error on picture delete: " + error.message);
+	        });
+		}
+		Picture.fileName = '';
+		Picture.subjects = [];
+		Picture.comment = '';
+		Picture.imgURI = '';
+		Picture.imgInfo = '';
+	};
+
+	//function edit picture info
+	Picture.edit = function (id){
+
+		//find picture object
+        var index = -1;
+        for(i = 0; i < Picture.pictures.length; i++) {
+            if (Picture.pictures[i].id == id) {
+                index = i;
+                break;
+            }
+        }
+        if(index > -1){
+        	//update picture
+            Picture.fileName = Picture.pictures[i].fileName;
+			Picture.subjects = Picture.pictures[i].subjects;
+			Picture.comment = Picture.pictures[i].comment;
+			Picture.imgURI = Picture.pictures[i].imgURI;
+			Picture.imgInfo = Picture.pictures[i].imgInfo;
+
+			//remove from list
+			Picture.pictures.splice(index, 1);
+
+			//remove from DB
+			//remove from DB
+	        $cordovaSQLite.execute(db, 'DELETE FROM logs WHERE log_id = (?)',[id])
+	        .then(function(result) {
+	            console.log("Picture delete successful");
+	        }, function(error) {
+	            console.log("Error on picture delete: " + error.message);
+	        });
+	        
+			return Picture.imgURI ;
+        }else{
+        	return "img/pic_placeholder.png";
+        }
+	};
+
+	//return picture object
+	return Picture;
+});
